eae42862ed71996b45f5a30d1b85bcdc8e1770b4
Seikinomori <133923523+Seikinomor@users.noreply.github.com>
2023-06-08 20:50:10 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 import os
-import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
@@ -16,45 +15,29 @@ def index():
             file.save(filepath)
             return redirect(url_for('visualize'))
 
-    # Завантаження даних з файлу з правильним роздільником
-    data = pd.read_csv("film.csv", encoding='latin-1', delimiter=';')
+    return render_template('index.html')
 
-    # Отримання фільмів, у яких знімалася Bardot Brigitte
-    bardot_movies = data[data['Actress'] == 'Bardot, Brigitte']
-
-    # Групування фільмів за роком випуску та підрахунок кількості фільмів за кожен рік
-    bardot_movies_by_year = bardot_movies.groupby('Year').size()
-
-    # Виведення назв фільмів актриси Brigitte Bardot по роках у консоль
-    for year, movie_count in bardot_movies_by_year.items():
-        movies = bardot_movies[bardot_movies['Year'] == year]['Title'].tolist()
-        print(f"Рік: {year}, Кількість фільмів: {movie_count}, Фільми: {', '.join(movies)}")
-
-    # Побудова графіка
-    plt.plot(bardot_movies_by_year.index, bardot_movies_by_year.values)
-    plt.xlabel('Year')
-    plt.ylabel('Number of Films')
-    plt.title('Bardot Brigitte Movies by Year')
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    plt.savefig('static/bardot_movies_plot.png')  # Зберегти графік у папці 'static'
-    plt.close()
-
-    return render_template('index.html', bardot_movies=bardot_movies)
-
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = 'film.csv'
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        return redirect(url_for('visualize'))
+    return redirect(url_for('index'))
 
 @app.route('/visualize')
 def visualize():
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], 'film.csv')
     if os.path.exists(filepath):
-        data = pd.read_csv(filepath, encoding='latin1', sep=';')
-        return render_template('visualize.html', data=data.to_html())
-    return render_template('visualize.html', data=None)
+        data = pd.read_csv(filepath, encoding='latin1', delimiter=';')
 
+        return render_template('visualize.html', data=data.to_dict('records'))
+    return render_template('visualize.html', data=None)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() == 'csv'
-
 
 if __name__ == '__main__':
     app.run(debug=False, threaded=True, port=8002)
